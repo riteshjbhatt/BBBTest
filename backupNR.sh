bf571d2f083aded0d460d9b70e9bb40b16d178f9
BeagleBone <72875877+riteshjbhatt@users.noreply.github.com>
2021-06-21 17:59:59 -0400

    NodeRed Backups

--- backupNR.sh
+++ backupNR.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cp /var/lib/node-red/.node-red/flows_beaglebone.json /home/debian
+today=$(date +"_%y%m%d_%H%M%S")
+echo $today
+echo
+filename="/home/debian/nodeRedBackups/flows_beaglebone$today.json"
+echo $filename
+echo
+cp -f /var/lib/node-red/.node-red/flows_beaglebone.json $filename
